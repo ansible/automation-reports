4958848378e4f523a973a1a67d0b15ed83ac5879
Kristijan Čagran <kristijan.cagran@xlab.si>
2025-02-25 08:13:22 +0100

    Fix chart date range

--- src/backend/apps/clusters/helpers.py
+++ src/backend/apps/clusters/helpers.py
@@ -151,7 +151,6 @@ def get_chart_range(request):
         return result
 
     result["date_range"] = DateRangeChoices.get_date_range(_date_range, _start_date, _end_date)
-
     if result["date_range"] is None:
         return result
 
@@ -168,7 +167,7 @@ def get_chart_range(request):
             result["range"] = "year"
         case _:
             days = (result["date_range"].end - result["date_range"].start).days
-            if days <= 1:
+            if days < 1:
                 result["range"] = "hour"
                 result["date_format"] = "YYYY-MM-DD HH24:00:00+00"
             elif days <= 45:
@@ -197,7 +196,7 @@ def get_chart_x_axis(chart_range):
             d = start.replace(hour=i, minute=0, second=0, microsecond=0)
             result.append(d)
     elif x_axis_range == "day":
-        current_date = start.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        current_date = start.replace(hour=0, minute=0, second=0, microsecond=0)
         while current_date.date() <= end.date():
             result.append(current_date)
             _day = current_date.day + 1
