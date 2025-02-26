df046941224a8de45fcb98679fdbb043fc53f056
Justin Cinkelj <justin.cinkelj@xlab.si>
2025-02-26 12:45:54 +0100

    uwsgi, supervisor
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

--- docker/launch_automation_reporter.sh
+++ docker/launch_automation_reporter.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Script is started via dumb-init
 
+##!/usr/bin/env bash
+#if [ `id -u` -ge 500 ]; then
+#    echo "awx:x:`id -u`:`id -g`:,,,:/var/lib/awx:/bin/bash" >> /tmp/passwd
+#    cat /tmp/passwd > /etc/passwd
+#    rm /tmp/passwd
+#fi
+
 nginx -g 'daemon off;' &
 /venv/bin/python manage.py runserver 0.0.0.0:8000 &
 exec sleep inf
