7741264d887df358003b17ebec5691da71eedc81
Justin Cinkelj <justin.cinkelj@xlab.si>
2025-12-03 08:25:43 +0100

    Minimal mock test - only detect_aap_version is tested
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>
    
    # Conflicts:
    #	requirements_dev.txt

---
+++ src/backend/tests/mock_aap/test_minimal.py
@@ -0,0 +1,77 @@
+import pytest
+import responses
+
+from backend.apps.clusters.connector import ApiConnector
+from backend.apps.clusters.models import ClusterVersionChoices, Cluster
+from backend.apps.clusters.encryption import encrypt_value
+
+
+@pytest.fixture
+def cluster_26():
+    """Creates temporary data."""
+    Cluster.objects.create(
+        protocol="https",
+        address="aap26.example.com",
+        port=443,
+        access_token=encrypt_value(b"access-token-26"),
+        refresh_token=encrypt_value(b"refresh-token-26"),
+        client_id="client-id-26",
+        client_secret=encrypt_value(b"client-secret-26"),
+        verify_ssl=True,
+        # sync_schedules: List[SyncSchedule] | None = []
+        # aap_version: Literal["AAP 2.6", "AAP 2.5", "AAP 2.4"] | None = None
+        # api_url="aaa",
+        # base_url="",
+        aap_version="asdf",
+    )
+
+
+@pytest.fixture
+def aap_api_responses_26():
+    responses.add(
+        responses.GET,
+        "https://aap26.example.com:443/api/gateway/v1/ping/",
+        json={"status":"good","version":"2.6","pong":"2025-11-19 08:19:48.890695","db_connected":True,"proxy_connected":True},
+        status=200,
+        content_type='application/json'
+    )
+
+@pytest.fixture
+def aap_api_responses_25():
+    responses.add(
+        responses.GET,
+        "https://aap25.example.com:443/api/gateway/v1/ping/",
+        json={"version":"2.5","pong":"2025-11-19 09:38:32.398165","status":"good","db_connected":True,"proxy_connected":True},
+        status=200,
+        content_type='application/json'
+    )
+
+@pytest.fixture
+def aap_api_responses_24():
+    responses.add(
+        responses.GET,
+        "https://aap24.example.com:8443/api/gateway/v1/ping/",
+        json={"detail":"The requested resource could not be found."},
+        status=404,
+        content_type='application/json'
+    )
+    responses.add(
+        responses.GET,
+        "https://aap24.example.com:8443/api/v2/ping/",
+        json={"ha":False,"version":"4.5.15","active_node":"rh-savings-aap24.novalocal","install_uuid":"4994da8f-a38f-4610-a6c3-ef410cbd2e2e","instances":[{"node":"rh-savings-aap24.novalocal","node_type":"hybrid","uuid":"eba627ff-78dd-4aaa-8b46-6e539880b808","heartbeat":"2025-11-19T09:40:51.214112Z","capacity":136,"version":"4.5.15"}],"instance_groups":[{"name":"controlplane","capacity":136,"instances":["rh-savings-aap24.novalocal"]},{"name":"default","capacity":136,"instances":["rh-savings-aap24.novalocal"]}]},
+        status=404,
+        content_type='application/json'
+    )
+    
+
+# How to "combine" smaller fixtures - syntax suygar
+# https://smarie.github.io/python-pytest-cases/pytest_goodies/#unpack_fixture-unpack_into
+
+@pytest.mark.django_db(transaction=True, reset_sequences=True)
+@pytest.mark.usefixtures("cluster_26", "aap_api_responses_26")
+class TestApiConnector:
+    def test_detect_aap_version(self):
+        cluster = Cluster.objects.get(address="aap26.example.com")
+        apiconn = ApiConnector(cluster)
+        aap_version = apiconn.detect_aap_version()
+        assert aap_version == ClusterVersionChoices.AAP26
