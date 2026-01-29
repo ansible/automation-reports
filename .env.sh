c573d267d7f4d4d67af1eb038a4d3a7b7f9f3ae7
Justin Cinkelj <justin.cinkelj@xlab.si>
2026-01-29 18:36:50 +0100

    AAP-62164 .env files must not be a shell script (#212)
    * Make .env file an .env file
    
    docker compose will need use .env file, and this requires a real .env file
    Not a shell script.
    Move allexport to separate .env.sh - bash script.
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>
    
    * Simplify common terminal setup for easier testing
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>
    
    ---------
    
    Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>

---
+++ .env.sh
@@ -0,0 +1,33 @@
+# Save current allexport state and enable it
+push_allexport() {
+    case "$(set -o | grep allexport)" in
+        *on)  __old_allexport=on  ;;
+        *off) __old_allexport=off ;;
+    esac
+    set -o allexport
+}
+
+# Restore previous allexport state
+pop_allexport() {
+    if [ "$__old_allexport" = on ]; then
+        set -o allexport
+    else
+        set +o allexport
+    fi
+    unset __old_allexport
+}
+
+# we do "source this_script.sh"
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+pushd "$SCRIPT_DIR"
+# Load environment variables
+push_allexport
+source .env
+pop_allexport
+# Activate virtual environment
+source .venv/bin/activate
+# Set Python path
+export PYTHONPATH=$PWD/src
+popd
+
+echo DB_NAME=$DB_NAME
